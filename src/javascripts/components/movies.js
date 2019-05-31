8875cc7e806d692fe9212eaf37d187d390ebad4d
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-05-31 14:02:54 -0500

    fixed issue where overlay turned off when hovering over overlay title

--- src/javascripts/components/movies.js
+++ src/javascripts/components/movies.js
@@ -7,9 +7,9 @@ const overlayEvents = () => {
   $('.card-img-overlay').hide();
   $('.movie-image').mouseover((event) => {
     const overlayId = event.target.nextElementSibling.id;
-    $(`#${overlayId}`).show();
-    $(`#${overlayId}`).mouseout(() => {
-      $(`#${overlayId}`).hide();
+    $(`#${overlayId}`).fadeIn();
+    $(`#${overlayId}`).mouseleave(() => {
+      $(`#${overlayId}`).fadeOut();
     });
   });
 };
@@ -17,10 +17,10 @@ const overlayEvents = () => {
 const writeMovies = (array) => {
   let domString = '';
   array.forEach((item) => {
-    domString += '<div class="col-3 p-3">';
+    domString += '<div class="col-3 p-3 movie-card">';
     domString += `  <img id=${item.id} src=${item.image} class="d-block w-100 movie-image" alt="movie cover of ${item.title}">`;
     domString += `  <div id="${item.id}-overlay" class="card-img-overlay bg-overlay m-3">`;
-    domString += `    <h5 class="text-white">${item.title}</h5>`;
+    domString += `    <h5 class="overlay-title text-white">${item.title}</h5>`;
     domString += '  </div>';
     domString += '</div>';
   });
